05c945ee4e35942f152cdab8587794dc7258aad4
bertin <ruhimbazab@gmail.com>
2021-01-21 22:13:44 -0800

    setting project up
    adding some files and folders in project
    
    blog project

---
+++ src/components/PostList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class PostList extends React.Component{
+
+    render(){
+        return(
+            <div>
+                PostList
+              </div>
+        )
+    }
+}
+
+export default PostList;
---
+++ src/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux';
+
+export default combineReducers({
+
+    replaceMeBack: () => 'I will be back to change you with valid reducers'
+});
