047aee01da6ba37c598402a28c28a900571017e3
bertin <ruhimbazab@gmail.com>
2021-01-25 02:17:03 -0800

    new UserHeader component
    adding userHeader component
    adding userReducer file in reducer folder
    
    new component

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,3 +5,9 @@ export const fetchPosts = () => async dispatch => {
 
     dispatch({type: 'FETCH_POSTS', payload: response.data})
 } 
+
+export const fetchUser = id => async dispatch => {
+    const response = await jsonPlaceHolder.get(`/users/${id}`);
+    console.log("This is the response from Action",response);
+    dispatch({type: 'FETCH_USER', payload: response.data});
+};
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 import  postReducer from '../reducers/postReducer';
+import usersReducer from '../reducers/usersReducer';
 
 export default combineReducers({
 
     posts: postReducer,
+    users: usersReducer,
 });
