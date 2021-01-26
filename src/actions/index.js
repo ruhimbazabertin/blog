2f37638e4067e23529742daebe01c94e756574de
bertin <ruhimbazab@gmail.com>
2021-01-26 08:16:46 -0800

    optimizing blog
    exploring lodash methods like
    chain
    map
    uniq
    
    lodash

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,20 @@
 import _ from 'lodash';
 import jsonPlaceHolder from '../api/jsonPlaceHolder';
 
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+    await dispatch(fetchPosts());
+    //const userIds = _.uniq(_.map(getState().posts, 'userId'));
+    //userIds.forEach(id => dispatch(fetchUser(id)));
+    
+    //ALTERNATIVE WAY using CHAIN LODASH
+
+    _.chain(getState().posts)
+    .map('userId')
+    .uniq()
+    .forEach(id => dispatch(fetchUser(id)))
+    .value();
+
+}
 
 export const fetchPosts = () => async dispatch => {
     const response = await jsonPlaceHolder.get('/posts');
@@ -8,8 +22,13 @@ export const fetchPosts = () => async dispatch => {
     dispatch({type: 'FETCH_POSTS', payload: response.data})
 } 
 
- export const fetchUser = id => dispatch =>  _fetchUser(id, dispatch);
- const _fetchUser = _.memoize ( async (id, dispatch ) => {
+ export const fetchUser = id =>  async dispatch => {
     const response = await jsonPlaceHolder.get(`/users/${id}`);
     dispatch({type: 'FETCH_USER', payload: response.data});
- });
+ };
+
+//  export const fetchUser = id => dispatch =>  _fetchUser(id, dispatch);
+//  const _fetchUser = _.memoize ( async (id, dispatch ) => {
+//     const response = await jsonPlaceHolder.get(`/users/${id}`);
+//     dispatch({type: 'FETCH_USER', payload: response.data});
+//  });
--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { fetchUser } from '../actions';
 
 class UserHeader extends React.Component {
     componentDidMount(){
-    this.props.fetchUser(this.props.userId);
     }
 
     render(){
@@ -24,5 +22,4 @@ const mapStateToProps = (state, ownProps) => {
 
 export default connect(
     mapStateToProps,
-         { fetchUser }
     )(UserHeader);
